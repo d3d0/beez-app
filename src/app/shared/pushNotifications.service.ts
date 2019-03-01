f453051c5584e08ab4641ab84215b21f60ab743c
Mirko Sacchetti <ms@random.cards>
2019-03-01 19:47:25 +0100

    notification

---
+++ src/app/shared/pushNotifications.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, NgZone} from "@angular/core";
+import { HttpHeaders, HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, BehaviorSubject, throwError } from "rxjs";
+import { localize } from "nativescript-localize";
+import { map, catchError, first, retry } from "rxjs/operators";
+import { BackendService } from "../shared/backend.service";
+import { ObservableArray } from "tns-core-modules/data/observable-array";
+import { filter } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: "root"
+})
+
+export class PushNotificationsService {
+
+
+  constructor( private http: HttpClient, private zone:NgZone ) {}
+
+  push_token(token) {
+    return this.http.post(
+      BackendService.baseUrl + 'beez/push_notifications',
+      JSON.stringify({
+        token: token,
+        type: 'android'
+      }),
+      {
+        headers: BackendService.getCommonHeaders()
+      })
+    }
+
+   
+    private handleErrors(error: Response){
+      if (error.status == 407){
+        BackendService.reset()
+        alert(localize("MESSAGES.CONFIRM_EMAIL"));
+      }
+      return throwError(error);
+    }
+
+    private getCommonHeader(){
+      return new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+        'X-CSRF-Token': BackendService.XCSFRtoken,
+        'Cookie': BackendService.session_name + "=" + BackendService.sessid
+      })
+    }
+  }
