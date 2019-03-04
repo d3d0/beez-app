7b8af532b638962e07e07ee911ce5a478b45ab0c
Mirko Sacchetti <ms@random.cards>
2019-03-04 17:48:18 +0100

    delete notification action

--- src/app/notifications/notifications-list/notifications-list.component.ts
+++ src/app/notifications/notifications-list/notifications-list.component.ts
@@ -1,15 +1,13 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
-import { ListViewEventData } from "nativescript-ui-listview";
-import { RouterExtensions } from "nativescript-angular/router";
-import { ActivatedRoute } from "@angular/router";
+import { RadListView, SwipeActionsEventData, ListViewEventData } from "nativescript-ui-listview";
+import { RouterExtensions } from "nativescript-angular/router"; import { ActivatedRoute } from "@angular/router";
 import { NotificationsService} from "../notifications.service";
 import { Notification} from "../notification.model";
 import { Router } from "@angular/router";
-
 import { View } from "ui/core/view";
-
 import {Color} from "color";
 import {isIOS} from "platform" ;
+import { topmost } from "tns-core-modules/ui/frame";
 
 @Component({
   selector: 'ns-notifications',
@@ -20,57 +18,66 @@ import {isIOS} from "platform" ;
 export class NotificationsListComponent implements OnInit {
 
   notifications:any;
+  @ViewChild("delete-view") deleteView: View;
 
   constructor(
     private activeRoute: ActivatedRoute,
     private routerExtensions: RouterExtensions,
     private router: Router,
-    private store: NotificationsService) {
-    store.load().subscribe(
-    notifications => {
-      console.log(notifications)
-      this.notifications = notifications}
-      )
+    private notificationService: NotificationsService) {
+    notificationService.load().subscribe(
+      notifications => {
+        console.log(notifications)
+        this.notifications = notifications}
+        )
   }
 
   ngOnInit() {
   	console.log('hello from Notifications component');
   }
 
-  goToCasting(id){
-    if( id )this.router.navigate(["../../castings/casting", id], { relativeTo: this.activeRoute })
+  goToCasting(notification){
+
+    if (!notification.read) this.notificationService.setRead(notification.mid).subscribe(result=>console.log("notification.read OK ",result))
+    if( notification.id )  { }
+        // this.router.navigate(['./', { outlets: { castingsTab: ['casting', notification.id] } }], { relativeTo: this.activeRoute })
+      this.router.navigate(["../casting", notification.id], { relativeTo: this.activeRoute })
   }
+
   // public onPullToRefreshInitiated(args: ListViewEventData) {
-  //   this.store.load().subscribe(
-  //   notifications => {
-  //     console.log(notifications)
-  //     this.notifications = notifications
-  //   }
-  //     )
-  // }
-  
-  // DOCS > eliminiamo il background solo in IOS RadListView
-  public onItemLoading(args: ListViewEventData){
+    //   this.store.load().subscribe(
+    //   notifications => {
+      //     console.log(notifications)
+      //     this.notifications = notifications
+      //   }
+      //     )
+      // }
+
+      // DOCS > eliminiamo il background solo in IOS RadListView
+      public onItemLoading(args: ListViewEventData){
         console.log("onItemLoading");
         if(isIOS){
-            console.log(args.ios);
-            var newcolor = new Color(0,0,0,0);
-            args.ios.backgroundView.backgroundColor = newcolor.ios;
+          console.log(args.ios);
+          var newcolor = new Color(0,0,0,0);
+          args.ios.backgroundView.backgroundColor = newcolor.ios;
         }
-  }
+      }
 
-  // DOCS > implementazione swipe in base a larghezza di swipe template
-  public onSwipeCellStarted(args: ListViewEventData) {
-    const swipeLimits = args.data.swipeLimits;
-    const swipeView = args['object'];
-    const rightItem = swipeView.getViewById<View>('delete-view');
-    swipeLimits.left = 0;
-    swipeLimits.right = rightItem.getMeasuredWidth();
-    swipeLimits.threshold = rightItem.getMeasuredWidth() / 2;
-  }
+      // DOCS > implementazione swipe in base a larghezza di swipe template
+      public onSwipeCellStarted(args: ListViewEventData) {
+        const swipeLimits = args.data.swipeLimits;
+        const swipeView = args['object'];
+        const rightItem = swipeView.getViewById<View>('delete-view');
+        swipeLimits.left = 0;
+        swipeLimits.right = rightItem.getMeasuredWidth();
+        swipeLimits.threshold = rightItem.getMeasuredWidth() / 2;
+      }
 
-  // DOCS > funzione per eliminare la notifica
-  public onRightSwipeClick(args) {
-      console.log("Right swipe click");
-  }
-}
+      // DOCS > funzione per eliminare la notifica
+      public onRightSwipeClick(args) {
+        console.log("args.object.bindingContext",args.object.bindingContext)
+        let data = this.notifications.splice(this.notifications.indexOf(args.object.bindingContext), 1)
+        console.log("data.mid ",data[0].mid)
+        this.notificationService.delete(data[0].mid).subscribe(result=>console.log(result))
+      }
+    }
--- src/app/notifications/notifications.service.ts
+++ src/app/notifications/notifications.service.ts
@@ -13,9 +13,24 @@ export class NotificationsService {
   constructor(private http: HttpClient) {}
 
   load() {
-    //   return Notifications
     return this.http.get(BackendService.baseUrl + 'beez/messages-notification', {
       headers: BackendService.getCommonHeaders()
     })
   }
-}
+  setRead(mid) {
+      return this.http.post( BackendService.baseUrl + "beez/messages-notification/"+mid,
+      	{} ,
+      {
+        headers: BackendService.getCommonHeaders()
+      });
+  }
+  delete(mid) {
+      return this.http.post( BackendService.baseUrl + "beez/messages-notification/hide",
+      	{
+      		"mid":mid
+      	} ,
+      {
+        headers: BackendService.getCommonHeaders()
+      });
+  }
+  }
