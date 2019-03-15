d0532375ab856185455e2895b945f7633b6ebde0
Mirko Sacchetti <ms@random.cards>
2019-03-15 22:15:52 +0100

    upload in base64

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -29,4 +29,44 @@ export class ProfileService {
         headers: BackendService.getCommonHeaders()
       });
   }
+
+    drupal_upload(filename,file){
+        return this.http.post( BackendService.baseUrl + "beez/loool_talent_profile/media_images_upload",
+            {
+                "filename":filename,
+                "file":file,
+                'mimeType': 'image/jpeg',
+                "uid":BackendService.UID
+            } ,
+            {
+                headers: BackendService.getCommonHeaders()
+            });
+        }
+
+  getImages() {
+    return this.http.get(BackendService.baseUrl + 'beez/loool_talent_images/'+ BackendService.UID,
+    {
+      headers: BackendService.getCommonHeaders()
+    })
+  }
+  deleteImage(fid) {
+    return this.http.post(BackendService.baseUrl + 'beez/loool_talent_images/deleteimage',
+    {
+      "uid": BackendService.UID,
+      "fid": fid
+    },
+    {
+      headers: BackendService.getCommonHeaders()
+    })
+  }
+  setPolaroidImage(fid) {
+    return this.http.post(BackendService.baseUrl + 'beez/loool_talent_images/setpolaroid',
+    {
+      "uid": BackendService.UID,
+      "fid": fid
+    },
+    {
+      headers: BackendService.getCommonHeaders()
+    })
+  }
 }
