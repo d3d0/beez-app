0e7d04e17d898ce29a6d33ac4e865710e783e2e2
d3d0 <lorenzo.dedonato@gmail.com>
2019-10-30 14:42:13 +0100

    test tabview badge

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,7 @@
-import { Component, ViewContainerRef, OnInit } from "@angular/core";
+import { Component, ViewChild, ElementRef, ViewContainerRef, OnInit, AfterViewInit } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
 import { ModalDialogService, ModalDialogOptions } from "nativescript-angular/directives/dialogs";
 import { RouterExtensions } from "nativescript-angular/router";
-import { isAndroid } from "tns-core-modules/platform";
 import { Page } from "ui/page";
 import { localize } from "nativescript-localize";
 import { EventData } from "tns-core-modules/data/observable";
@@ -11,6 +10,11 @@ import { AppModule } from "../app.module";
 import { BackendService } from "../shared/backend.service";
 import { TaxonomyService } from "../shared/taxonomy.service";
 import { MessageModalViewComponent } from "../components/message-modal-view/message-modal-view.component";
+
+import * as app from "tns-core-modules/application";
+import {isIOS, isAndroid} from "tns-core-modules/platform";
+
+
 
 export function onLayoutChanged(args: EventData) {
   console.log(args.eventName);
@@ -22,7 +26,9 @@ export function onLayoutChanged(args: EventData) {
   styleUrls: ['./home.component.scss'],
   moduleId: module.id,
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  @ViewChild("homeTabs", { static: false }) homeTabs: ElementRef;
 
   constructor(
     private routerExtension: RouterExtensions,
@@ -32,11 +38,19 @@ export class HomeComponent implements OnInit {
     private vcRef: ViewContainerRef,
     private modal: ModalDialogService,
     private page: Page) {
-    /// warmup taxonomyService
-    this.taxonomyService.load()
+    // warmup taxonomyService
+    this.taxonomyService.load();
   }
 
   ngOnInit(): void {
+
+    // this.homeTabs = this.homeTabs.nativeElement;
+    console.log('Loading homeTabs', this.homeTabs);
+
+    // var myTabView = page.getViewById("myTabView");
+    // var tabItem = myTabView.ios.tabBar.items[0];
+    // tabItem.badgeValue = "5";
+
     this.routerExtension.navigate([{ outlets: { castingsTab: ["castings"], notificationsTab: ["notifications"], profileTab: ["profile"] } }], { relativeTo: this.activeRoute });
     this.page.actionBarHidden = true;
 
@@ -51,6 +65,17 @@ export class HomeComponent implements OnInit {
       })
     }
     
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit homeTabs: " + this.homeTabs.nativeElement);
+    if (isIOS) {
+      // var tabItem = this.homeTabs.nativeElement.ios.tabBar.items[0];
+      // var tabItem = this.homeTabs.nativeElement.ios.tabBar.items;
+      // var tabItem = this.homeTabs.nativeElement.ios.tabBar;
+      // console.log("ngAfterViewInit homeTabs tabItem: " + tabItem);
+      // tabItem.badgeValue = "1";
+    }
   }
 
   getIconSource(icon: string): string {
