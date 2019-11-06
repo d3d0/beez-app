645357e6624f5bbc74a6d6d110373891e2b8b844
Francesca Suprani <francesca@Francescas-MacBook-Pro.local>
2019-11-06 19:39:28 +0100

    recovery component

---
+++ src/app/user/recovery/recovery.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, NgZone} from "@angular/core";
+import { HttpHeaders, HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, BehaviorSubject, throwError } from "rxjs";
+import { localize } from "nativescript-localize";
+import { map, catchError, first, retry } from "rxjs/operators";
+import { BackendService } from "../../shared/backend.service";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RecoveryService {
+
+  constructor(private http: HttpClient) { }
+  
+  private handleErrors(error: Response): Observable<never> {
+    if (error.status == 407){
+      BackendService.reset()
+      return
+    }
+    return throwError(error);
+  }
+
+
+  recoveryPwd(name){
+    return this.http.post( BackendService.baseUrl + "beez/user/request_new_password",{'name': name },{});
+  }
+
+
+
+  
+  
+
+
+}
